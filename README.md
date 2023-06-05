3d8c2ccb1b5c9236402d9864fe1281895f1b5917
jean <jeanribeiro1813@gmail.com>
2023-06-05 18:57:59 -0300

    feat:Ultima atualizacao Cod

--- README.md
+++ README.md
@@ -7,8 +7,11 @@
 Não sei bem como é o formato que vcs recebem ou gosta de receber então fiz de uma maneira que geralmente gosto de fazer que é separação por '|',
 enfim , foi gerado o requerimentos para ser instalado para que o programa funcione 
 
+É necessario ter o arquivo do 'driver' atualizado com a ultima versão do navegador , estou utilizando o google , e precisa colocar dentro do codigo -->  webdriver.Chrome('caminho onde esta o executavel do driver') se não o codigo não funciona
+
  **pip install -r requirements.txt **
  
+Para executar o mesmo , simplesmente "python nomeArquivo.py"
 Fiz da maneira mais rapida e pratica claro que no dia a dia por se tratar de capturas com mais informações o formato do codigo é diferente,
 porém tentei trazer agilidade e geralmente com um formato mais visivel
 
--- crawlerCruzeiro.py
+++ crawlerCruzeiro.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from unidecode import unidecode
 
+##Utilizando Selenium lembrando que no computador é necessario o chromedriver para executar
 driver = webdriver.Chrome(
             'D:/Download/driver/chromedriver.exe')
 
@@ -14,18 +16,19 @@ driver.get('https://estrelabet.com/ptb/bet/search/Cruzeiro')
 
 time.sleep(15)
 
+## Pegando as informações principais
 cruzeiro = driver.find_element(By.CSS_SELECTOR,'#container-main > app-fixture-search > div.srch.page > div.modul-accordion.sportType > div.modul-content > div:nth-child(1) > div.modul-content').text.split('\n')
 
-data = cruzeiro[4]
+## Fazendo a semparação de casa informação , para deixar de uma maneira mais organizada
+data = unidecode(cruzeiro[4])
 horario = cruzeiro[5]
-
 adiversario = cruzeiro[6]
 principal = cruzeiro[7]
-
 vitoriaBh = ' - '.join(cruzeiro[10:12])
-
 empate = ' - '.join(cruzeiro[12:14])
 vitoria_cruz=' - '.join(cruzeiro[14:16])
+
+#Criando um arquivo csv com as informações
 with open ('CruzeiroValor.csv','w') as dados:
         
     print(f"{data} - {horario}|{adiversario} X {principal}|Vitoria - {vitoriaBh} |{empate}|Vitoria - {vitoria_cruz}\n")
